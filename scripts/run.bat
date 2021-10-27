76eb32102a2ec4de91b079cf19f508ab78a4b83f
Jen-Chieh Shen <jcs090218@gmail.com>
2021-10-27 18:24:12 +0800

    Add script

---
+++ scripts/run.bat
@@ -0,0 +1,13 @@
+@echo off
+:: ========================================================================
+:: $File: run.bat $
+:: $Date: 2021-10-17 19:58:36 $
+:: $Revision: $
+:: $Creator: Jen-Chieh Shen $
+:: $Notice: See LICENSE.txt for modification and distribution information
+::                   Copyright © 2021 by Shen, Jen-Chieh $
+:: ========================================================================
+
+cd ..
+
+cargo build
